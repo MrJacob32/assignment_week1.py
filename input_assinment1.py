43c4f0cedca573d29d70540180daec6f7f44466b
MrJacob32 <159633193+MrJacob32@users.noreply.github.com>
2024-02-27 02:43:39 +0300

    Add files via upload

---
+++ input_assinment1.py
@@ -0,0 +1,9 @@
+# defining variables and input functions
+
+name = input( 'what is your name:' )
+age = input('how old are you:' )
+location = input('where do you live:' )
+
+#printing the personalized message
+print('Hi!', name, ',your age is ', age , ', you are living at', location )
+
